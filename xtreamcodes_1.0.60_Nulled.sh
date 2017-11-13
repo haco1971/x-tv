537f52be9666801506827d85cda9a7912ff86383
haco1971 <omsameh2010@gmail.com>
2017-11-14 06:14:14 +0700

    Update xtreamcodes_1.0.60_Nulled.sh

--- xtreamcodes_1.0.60_Nulled.sh
+++ xtreamcodes_1.0.60_Nulled.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting ffmpeg...\n\n"
-echo shell_exec("ffmpeg -i http://37.9.0.201:8888 -vcodec libx264 -acodec aac -f flv rtmp://80.211.154.228:1935/sport/camera1.stream )
-echo shell_exec("ffmpeg -i http://37.9.0.201:8889 -vcodec libx264 -acodec aac -f flv rtmp://80.211.154.228:1935/sport/camera2.stream )
-echo shell_exec("ffmpeg -i http://37.9.0.201:8890 -vcodec libx264 -acodec aac -f flv rtmp://80.211.154.228:1935/sport/camera3.stream )
+ffmpeg -i http://37.9.0.201:8888 -vcodec libx264 -acodec aac -f flv rtmp://80.211.154.228:1935/sport/camera1.stream 
+ffmpeg -i http://37.9.0.201:8889 -vcodec libx264 -acodec aac -f flv rtmp://80.211.154.228:1935/sport/camera2.stream 
+ffmpeg -i http://37.9.0.201:8890 -vcodec libx264 -acodec aac -f flv rtmp://80.211.154.228:1935/sport/camera3.stream 
 echo "Done.\n"
